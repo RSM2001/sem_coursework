15bdb361ebf691a9e68137f2914e9a03dd6c617f
Lewis Mackenzie <lewisstevenmackenzie@gmail.com>
2021-02-22 11:36:33 +0000

    added sql 3 + 4

---
+++ sqlqueries/4.sql
@@ -0,0 +1,5 @@
+--The top N populated countries in the world where N is provided by the user.
+SELECT Code, Name, Continent, Region, Population, Capital
+FROM country
+ORDER BY Population DESC
+LIMIT n
