1bcc700b06b6e811fa06689cba9a1ff47bcbe6e2
Robbie Mitchell <40429697@live.napier.ac.uk>
2021-02-24 11:45:24 +0000

    fixes to 10 and 18

--- sqlqueries/10.sql
+++ sqlqueries/10.sql
@@ -1,6 +1,6 @@
 --All the cities in a country organised by largest population to smallest
 SELECT city.name, country.name, city.district, city.population 
 FROM city JOIN country ON city.countrycode=country.code
-WHERE country = 'France'
+WHERE country.name = 'France'
 ORDER BY population DESC
 ;
