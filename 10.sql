3e21562d845bebec90c774080515e84d3793230d
RSM2001 <78076263+RSM2001@users.noreply.github.com>
2021-02-24 11:33:22 +0000

    Delete 10.sql

--- 10.sql
+++
@@ -1,6 +0,0 @@
---All the cities in a country organised by largest population to smallest
-SELECT city.name, country.name, city.district, city.population 
-FROM city JOIN country ON city.countrycode=country.code
-WHERE country = 'France'
-ORDER BY population DESC
-;
