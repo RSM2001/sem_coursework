7f2c69f4e44a77c32a22183042cd602fcade0000
RSM2001 <78076263+RSM2001@users.noreply.github.com>
2021-02-24 11:33:30 +0000

    Delete 11.sql

--- 11.sql
+++
@@ -1,6 +0,0 @@
---All the cities in a district organised by largest population to smallest
-SELECT city.name, country.name, city.district, city.population 
-FROM city JOIN country ON city.countrycode=country.code
-WHERE district = 'Bretagne'
-ORDER BY population DESC
-;
