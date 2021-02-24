4b52bb32540aa7c77a91f6f81af1efb036e0b6c2
lewisstevenmackenzie <64756559+lewisstevenmackenzie@users.noreply.github.com>
2021-02-24 12:41:17 +0000

    Update 16.sql
    updated format

--- sqlqueries/16.sql
+++ sqlqueries/16.sql
@@ -1,5 +1,7 @@
 --The top N populated cities in a continent where N is provided by the user.
-    SELECT city.name, country.name, city.district, city.population FROM city JOIN country ON city.countryCode=country.code
-        WHERE continent = 'example'
-    ORDER BY population DESC
-    LIMIT n 
+SELECT city.name, country.name, city.district, city.population 
+FROM city JOIN country ON city.countryCode=country.code
+WHERE continent = 'example'
+ORDER BY population DESC
+LIMIT n 
+;
