b9f5e5e2d45bc6c1f3315feba0c2b9c7dbd17a7f
lewisstevenmackenzie <64756559+lewisstevenmackenzie@users.noreply.github.com>
2021-02-24 12:42:07 +0000

    Update 13.sql

--- sqlqueries/13.sql
+++ sqlqueries/13.sql
@@ -1,4 +1,5 @@
 --All the capital cities in the world organised by largest population to smallest.
-SELECT city.name, country.name, city.population FROM city JOIN country ON city.id=country.capital
+SELECT city.name, country.name, city.population 
+FROM city JOIN country ON city.id=country.capital
 ORDER BY population DESC
 ;
