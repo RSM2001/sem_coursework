66dbfd6f572e78035b13cd1024291d271edd687a
RSM2001 <78076263+RSM2001@users.noreply.github.com>
2021-02-24 11:33:38 +0000

    Delete 12.sql

--- 12.sql
+++
@@ -1,6 +0,0 @@
---The top N populated cities in the world
-SELECT city.name, country.name, city.district, city.population
-FROM city JOIN country ON city.countrycode=country.code
-ORDER BY population DESC
-LIMIT n 
-;
