062be99296bc9fdd90dd33180c64d8dbb6af205e
lewisstevenmackenzie <64756559+lewisstevenmackenzie@users.noreply.github.com>
2021-02-24 12:40:23 +0000

    Update 14.sql

--- sqlqueries/14.sql
+++ sqlqueries/14.sql
@@ -1,4 +1,6 @@
 --All the capital cities in a continent organised by largest population to smallest.
-    SELECT city.name, country.name, city.population FROM city JOIN country ON city.id=country.capital
-        WHERE continent = 'example'
-    ORDER BY population DESC
+SELECT city.name, country.name, city.population 
+FROM city JOIN country ON city.id=country.capital
+WHERE continent = 'Europe'
+ORDER BY population DESC
+;
