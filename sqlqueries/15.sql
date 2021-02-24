e532b78b289eb3584fb57db0e8ce48564ac0c1fd
lewisstevenmackenzie <64756559+lewisstevenmackenzie@users.noreply.github.com>
2021-02-24 12:40:53 +0000

    Update 15.sql

--- sqlqueries/15.sql
+++ sqlqueries/15.sql
@@ -1,4 +1,6 @@
 --All the capital cities in a region organised by largest to smallest.
-    SELECT city.name, country.name, city.population FROM city JOIN country ON city.id=country.capital
-        WHERE region = 'example'
-    ORDER BY population DESC
+SELECT city.name, country.name, city.population 
+FROM city JOIN country ON city.id=country.capital
+WHERE region = 'Western Europe'
+ORDER BY population DESC
+;
