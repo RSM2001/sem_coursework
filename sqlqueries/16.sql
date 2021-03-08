091d7ac8a2dced23d406f6d2bef147536e092fed
Lewis Mackenzie <lewisstevenmackenzie@gmail.com>
2021-03-08 12:49:34 +0000

    Added query 20. added a continent to query 16. fixed formatting on query 17.

--- sqlqueries/16.sql
+++ sqlqueries/16.sql
@@ -1,7 +1,7 @@
 --The top N populated cities in a continent where N is provided by the user.
 SELECT city.name, country.name, city.district, city.population 
 FROM city JOIN country ON city.countryCode=country.code
-WHERE continent = 'example'
+WHERE continent = 'Europe'
 ORDER BY population DESC
 LIMIT n 
 ;
