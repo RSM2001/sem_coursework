94b222e4d7710f8282ab3d8845fb9582ce5c23e6
RoanCreed7 <40430682@live.napier.ac.uk>
2021-03-14 21:45:45 +0000

    Adding query 25

---
+++ sqlqueries/25.sql
@@ -0,0 +1,4 @@
+SELECT country.name, sum(distinct country.population),  CONCAT(ROUND(sum(distinct city.population)/sum(distinct country.population)*100), '%'), CONCAT(ROUND((sum(distinct country.population)-sum(distinct city.population))/sum(distinct country.population)*100), '%')
+FROM city JOIN country ON city.countrycode=country.code
+GROUP BY Country.Name
+;
