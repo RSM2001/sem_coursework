36ae635e31500a1b290e732ec2fd6e1c2d513b07
RoanCreed7 <40430682@live.napier.ac.uk>
2021-03-14 21:55:31 +0000

    Updating query 25

--- sqlqueries/25.sql
+++ sqlqueries/25.sql
@@ -1,4 +1,4 @@
-SELECT country.name, sum(distinct country.population),  CONCAT(ROUND(sum(distinct city.population)/sum(distinct country.population)*100), '%'), CONCAT(ROUND((sum(distinct country.population)-sum(distinct city.population))/sum(distinct country.population)*100), '%')
+SELECT DISTINCT country.name, sum(distinct country.population),  CONCAT(ROUND(sum(distinct city.population)/sum(distinct country.population)*100), '%'), CONCAT(ROUND((sum(distinct country.population)-sum(distinct city.population))/sum(distinct country.population)*100), '%')
 FROM city JOIN country ON city.countrycode=country.code
-GROUP BY Country.Name
+GROUP BY name
 ;
