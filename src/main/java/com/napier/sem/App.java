337000a28ed58e191b04cc2d373de79250c1eeda
RoanCreed7 <40430682@live.napier.ac.uk>
2021-03-17 13:39:30 +0000

    Updating main app to allow integration testing

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -25,7 +25,7 @@ public class App
     /**
      * Connect to the MySQL database.
      */
-    public void connect ()
+    public void connect (String location)
     {
         try
         {
@@ -47,7 +47,7 @@ public class App
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://"+ location +"/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             }
@@ -209,7 +209,7 @@ public class App
         App a = new App();
 
         // Connect to database
-        a.connect();
+        a.connect("localhost:33060");
 
         // Execute SQL statements in SQLQueries directory
         for (int i = 1; i <= 32; i++)
